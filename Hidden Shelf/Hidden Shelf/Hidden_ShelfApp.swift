cccf2711149305ce2ee5ae81aa6ba70057390a94
Eileen <ecynthia01@student.ciputra.ac.id>
2026-05-29 08:26:39 +0700

    initial commit

---
+++ Hidden Shelf/Hidden Shelf/Hidden_ShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hidden_ShelfApp.swift
+//  Hidden Shelf
+//
+//  Created by student on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Hidden_ShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
